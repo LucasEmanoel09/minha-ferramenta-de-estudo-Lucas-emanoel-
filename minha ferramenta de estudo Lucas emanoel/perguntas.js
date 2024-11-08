71b833ae0688990b35b7ffd3f5c20e39e01e604f
LucasEmanoel09 <00001093208053sp@al.educacao.sp.gov.br>
2024-11-08 11:42:01 -0300

    Add files via upload

---
+++ minha ferramenta de estudo Lucas emanoel/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é free fire?',
+    'O free fire e um jogo que é uma linguagem de programação'
+)
+
+criaCartao(
+    'historia',
+    'Qual ano os inglases invadiram o brasil?',
+    'A inglaterra teve um presidente bom em 2015'
+)
+
+criaCartao(
+    'jogando um jogo daora tipo minecraft',
+    'ele e um jogo de sobrevivencia?',
+    'no minecraft vc pode chamar amigos pra jogar em muito mais'
+)
+
+criaCartao(
+    'Lingua ingles',
+    'Como falar carro em ingles?',
+    'carro em ingles e (car)'
+)
